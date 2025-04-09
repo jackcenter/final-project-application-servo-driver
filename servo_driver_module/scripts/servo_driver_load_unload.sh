5d64b6fa0219bd5c250f67b19015805a90bd7629
Jack <jack.d.center@gmail.com>
2025-04-09 09:39:34 -0600

    Update Load Script for Use on Startup

--- servo_driver_module/scripts/servo_driver_load_unload.sh
+++ servo_driver_module/scripts/servo_driver_load_unload.sh
@@ -5,43 +5,77 @@ device=servo_driver
 
 if ! [ $# -eq 1 ]
 then
-    echo "Usage: servo_driver_load_unload.sh {load|unload}"
+    echo "Usage: servo_driver_start_stop.sh {start|stop|restart|status}"
     exit 1
 fi
 
+# Loads the module
+start() {
+    mode="666"
+    set -e
+
+    # Group: since distributions do it differently, look for wheel or use staff
+    if grep -q '^staff:' /etc/group; then
+        group="staff"
+    else
+        group="wheel"
+    fi
+
+    if [ -e ${module}.ko ]; then
+        echo "Loading local built file ${module}.ko"
+        insmod $module.ko || exit 1
+    else
+        echo "Local file ${module}.ko not found, attempting to modprobe"
+        modprobe ${module} || exit 1
+    fi
+
+    if [ -e /dev/${device} ]; then
+        rm /dev/${device}
+    fi
+
+    major=$(awk "\$2==\"$device\" {print \$1}" /proc/devices)
+    mknod /dev/${device} c $major 0
+    chgrp $group /dev/${device}
+    chmod $mode  /dev/${device}
+}
+
+# Unloads the module
+stop() {
+    rmmod $module || exit 1
+    rm /dev/${device}
+}
+
+# Unloads then loads the module
+restart() {
+    stop
+    start
+}
+
+# Prints if the module is loaded or not
+status() {
+    if lsmod | grep "$module" > /dev/null; then
+        echo "$module is loaded"
+    else
+        echo "$module is not loaded"
+    fi
+}
+
 cd $(dirname $0)/..
 case "$argument" in
-    load)
-        mode="666"
-        set -e
-
-        # Group: since distributions do it differently, look for wheel or use staff
-        if grep -q '^staff:' /etc/group; then
-            group="staff"
-        else
-            group="wheel"
-        fi
-
-        if [ -e ${module}.ko ]; then
-            echo "Loading local built file ${module}.ko"
-            insmod $module.ko || exit 1
-        else
-            echo "Local file ${module}.ko not found, attempting to modprobe"
-            modprobe ${module} || exit 1
-        fi
-
-        major=$(awk "\$2==\"$device\" {print \$1}" /proc/devices)
-        rm /dev/${device}
-        mknod /dev/${device} c $major 0
-        chgrp $group /dev/${device}
-        chmod $mode  /dev/${device}
+    start)
+        start
         ;;
-    unload)
-        rmmod $module || exit 1
-        rm /dev/${device}
+    stop)
+        stop
+        ;;
+    restart)
+        restart
+        ;;
+    status)
+        status
         ;;
     *)
-        echo "Usage: servo-driver-load-unload {load|unload}"
+        echo "Usage: servo_driver_start_stop.sh {start|stop}"
         exit 1
         ;;
 esac
