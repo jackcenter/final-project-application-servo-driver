26f80c43eb7f5d9ee16f2fd2d593a2a1f5f899c6
Jack <jack.d.center@gmail.com>
2025-04-23 14:37:08 -0600

    Update Keyboard Inputs

--- servo_driver/src/main.c
+++ servo_driver/src/main.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <sys/select.h>
+#include <ctype.h>
 
 int main() {
     struct termios oldt, newt;
@@ -10,24 +11,39 @@ int main() {
     newt.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    printf("Listening for keypresses (q to quit)...\n");
+    printf("Listening for arrow keys and WASD (press 'q' to quit)...\n");
 
     while (1) {
         fd_set set;
         FD_ZERO(&set);
         FD_SET(STDIN_FILENO, &set);
 
-        struct timeval timeout = {1, 0}; // 1 second timeout
+        select(STDIN_FILENO + 1, &set, NULL, NULL, NULL);
 
-        int res = select(STDIN_FILENO + 1, &set, NULL, NULL, &timeout);
-        if (res > 0) {
-            char c = getchar();
-            printf("Pressed: %c\n", c);
-            if (c == 'q') break;
-        } else {
-            // Timeout hit, do background work if needed
-            printf(".");
-            fflush(stdout);
+        char seq[3];
+        if (read(STDIN_FILENO, &seq[0], 1) == 1) {
+            if (seq[0] == 'q' || seq[0] == 'Q') break;
+
+            if (seq[0] == '\x1b') { // Escape sequence for arrows
+                if (read(STDIN_FILENO, &seq[1], 1) == 1 && seq[1] == '[') {
+                    if (read(STDIN_FILENO, &seq[2], 1) == 1) {
+                        switch (seq[2]) {
+                            case 'A': printf("Up Arrow (pressed)\n"); break;
+                            case 'B': printf("Down Arrow (pressed)\n"); break;
+                            case 'C': printf("Right Arrow (pressed)\n"); break;
+                            case 'D': printf("Left Arrow (pressed)\n"); break;
+                        }
+                    }
+                }
+            } else {
+                switch (tolower(seq[0])) {
+                    case 'w': printf("W / Up (pressed)\n"); break;
+                    case 'a': printf("A / Left (pressed)\n"); break;
+                    case 's': printf("S / Down (pressed)\n"); break;
+                    case 'd': printf("D / Right (pressed)\n"); break;
+                    default: printf("Pressed: %c\n", seq[0]); break;
+                }
+            }
         }
     }
 
