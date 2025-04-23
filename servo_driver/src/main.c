a0cbbfb2946b790cf9adb4cb41fd5cc77e9ceec3
Jack <jack.d.center@gmail.com>
2025-04-23 14:02:17 -0600

    Add Keyboard Driver

--- servo_driver/src/main.c
+++ servo_driver/src/main.c
@@ -1,6 +1,36 @@
-#include "utilities.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/select.h>
 
-int main(void) {
-  utilities_print_hello_from("main");
-  return 0;
+int main() {
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    printf("Listening for keypresses (q to quit)...\n");
+
+    while (1) {
+        fd_set set;
+        FD_ZERO(&set);
+        FD_SET(STDIN_FILENO, &set);
+
+        struct timeval timeout = {1, 0}; // 1 second timeout
+
+        int res = select(STDIN_FILENO + 1, &set, NULL, NULL, &timeout);
+        if (res > 0) {
+            char c = getchar();
+            printf("Pressed: %c\n", c);
+            if (c == 'q') break;
+        } else {
+            // Timeout hit, do background work if needed
+            printf(".");
+            fflush(stdout);
+        }
+    }
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return 0;
 }
