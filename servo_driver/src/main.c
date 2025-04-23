4dd4a1657e62b6f511184bcc32b5c4e1de935fdd
Jack <jack.d.center@gmail.com>
2025-04-23 15:06:12 -0600

    Add Servo to Keyboard Program

--- servo_driver/src/main.c
+++ servo_driver/src/main.c
@@ -1,52 +1,93 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <termios.h>
-#include <sys/select.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <sys/select.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main() {
-    struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  struct termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    printf("Listening for arrow keys and WASD (press 'q' to quit)...\n");
+  int fd = open("/dev/servo_driver", O_WRONLY);
+  if (fd < 0) {
+    perror("Failed to open /dev/servo_driver");
+    return 1;
+  }
 
-    while (1) {
-        fd_set set;
-        FD_ZERO(&set);
-        FD_SET(STDIN_FILENO, &set);
+  printf("Listening for arrow keys and WASD (press 'q' to quit)...\n");
 
-        select(STDIN_FILENO + 1, &set, NULL, NULL, NULL);
+  while (1) {
+    fd_set set;
+    FD_ZERO(&set);
+    FD_SET(STDIN_FILENO, &set);
 
-        char seq[3];
-        if (read(STDIN_FILENO, &seq[0], 1) == 1) {
-            if (seq[0] == 'q' || seq[0] == 'Q') break;
+    select(STDIN_FILENO + 1, &set, NULL, NULL, NULL);
 
-            if (seq[0] == '\x1b') { // Escape sequence for arrows
-                if (read(STDIN_FILENO, &seq[1], 1) == 1 && seq[1] == '[') {
-                    if (read(STDIN_FILENO, &seq[2], 1) == 1) {
-                        switch (seq[2]) {
-                            case 'A': printf("Up Arrow (pressed)\n"); break;
-                            case 'B': printf("Down Arrow (pressed)\n"); break;
-                            case 'C': printf("Right Arrow (pressed)\n"); break;
-                            case 'D': printf("Left Arrow (pressed)\n"); break;
-                        }
-                    }
-                }
-            } else {
-                switch (tolower(seq[0])) {
-                    case 'w': printf("W / Up (pressed)\n"); break;
-                    case 'a': printf("A / Left (pressed)\n"); break;
-                    case 's': printf("S / Down (pressed)\n"); break;
-                    case 'd': printf("D / Right (pressed)\n"); break;
-                    default: printf("Pressed: %c\n", seq[0]); break;
-                }
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) == 1) {
+      if (seq[0] == 'q' || seq[0] == 'Q')
+        break;
+
+      if (seq[0] == '\x1b') { // Escape sequence for arrows
+        if (read(STDIN_FILENO, &seq[1], 1) == 1 && seq[1] == '[') {
+          if (read(STDIN_FILENO, &seq[2], 1) == 1) {
+            switch (seq[2]) {
+            case 'A':
+              printf("Up Arrow (pressed)\n");
+              break;
+            case 'B':
+              printf("Down Arrow (pressed)\n");
+              break;
+            case 'C': {
+              printf("Right Arrow (pressed)\n");
+              const char *data = "0"; // e.g., 75% duty cycle
+              if (write(fd, data, strlen(data)) < 0) {
+                perror("Failed to write to servo driver");
+                close(fd);
+                return 1;
+              }
+              break;
             }
+            case 'D': {
+              printf("Left Arrow (pressed)\n");
+              const char *data = "180"; // e.g., 75% duty cycle
+              if (write(fd, data, strlen(data)) < 0) {
+                perror("Failed to write to servo driver");
+                close(fd);
+                return 1;
+              }
+              break;
+            }
+            }
+          }
         }
+      } else {
+        switch (tolower(seq[0])) {
+        case 'w':
+          printf("W / Up (pressed)\n");
+          break;
+        case 'a':
+          printf("A / Left (pressed)\n");
+          break;
+        case 's':
+          printf("S / Down (pressed)\n");
+          break;
+        case 'd':
+          printf("D / Right (pressed)\n");
+          break;
+        default:
+          printf("Pressed: %c\n", seq[0]);
+          break;
+        }
+      }
     }
+  }
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    return 0;
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return 0;
 }
