5de7a1e96f232b7a788bf3f63ad2297fe90ace27
Jack <jack.d.center@gmail.com>
2025-04-28 15:21:07 -0600

    Make Devices Independent

--- servo_driver_module/servo_driver_module.c
+++ servo_driver_module/servo_driver_module.c
@@ -8,6 +8,7 @@
 #include <linux/uaccess.h>
 
 #include "log.h"
+#include "servo_device.h"
 #include "servo_ioctl.h"
 
 MODULE_AUTHOR("Jack Center");
@@ -15,12 +16,7 @@ MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("Creates a character driver for controlling a servo");
 
 #define DEVICE_NAME "servo_driver"
-
-static int major_number;
-static struct cdev servo_cdev;
-
-static struct pwm_device *pwm0;
-static struct pwm_device *pwm1;
+#define DEVICE_CNT 2
 
 static u32 pwm_period_ns = 20000000;
 static u32 pwm_duty_cycle_ns = 1500000;
@@ -68,73 +64,85 @@ static const struct file_operations servo_driver_fops = {
 module_platform_driver(pwm_driver); // Handles init and exit
 
 int pwm_probe(struct platform_device *pdev) {
-  dev_t dev;
-  int result;
-
   LOG_DEBUG("pwm_probe");
 
-  result = alloc_chrdev_region(&dev, 0, 2, DEVICE_NAME);
+  ServoDevice *servo_device;
+  servo_device = devm_kzalloc(&pdev->dev, sizeof(*servo_device), GFP_KERNEL);
+  if (!servo_device) {
+    return -ENOMEM;
+  }
+
+  servo_device->device = &pdev->dev;
+
+  int result =
+      alloc_chrdev_region(&servo_device->device_number, 0, DEVICE_CNT, DEVICE_NAME);
   if (result < 0) {
-    LOG_ERROR("Failed to allocate char device region");
+    LOG_ERROR("alloc_chrdev_region");
     return result;
   }
 
-  major_number = MAJOR(dev);
+  cdev_init(&servo_device->cdev, &servo_driver_fops);
+  servo_device->cdev.owner = THIS_MODULE;
 
-  cdev_init(&servo_cdev, &servo_driver_fops);
-  result = cdev_add(&servo_cdev, dev, 2);
+  result = cdev_add(&servo_device->cdev, servo_device->device_number, DEVICE_CNT);
   if (result < 0) {
-    LOG_ERROR("Failed to add cdev");
-    unregister_chrdev_region(dev, 2);
+    LOG_ERROR("cdev_add");
+    unregister_chrdev_region(servo_device->device_number, DEVICE_CNT);
     return result;
   }
 
-  pwm0 = devm_pwm_get(&pdev->dev, "pwm0");
-  if (IS_ERR(pwm0)) {
-    int err = PTR_ERR(pwm0);
+  servo_device->pwm[0] = devm_pwm_get(servo_device->device, "pwm0");
+  if (IS_ERR(servo_device->pwm[0])) {
+    int err = PTR_ERR(servo_device->pwm[0]);
     if (err == -EPROBE_DEFER) {
       LOG_WARN("PWM not ready, deferring probe 0");
-      unregister_chrdev_region(dev, 2);
+      unregister_chrdev_region(servo_device->device_number, DEVICE_CNT);
       return -EPROBE_DEFER;
     }
-    LOG_ERROR("Failed to get PWM 0: %d", err);
-    unregister_chrdev_region(dev, 2);
+    LOG_ERROR("Failed to get PWM: %d", err);
+    unregister_chrdev_region(servo_device->device_number, DEVICE_CNT);
     return err;
   }
+  pwm_config(servo_device->pwm[0], pwm_duty_cycle_ns, pwm_period_ns);
 
-  pwm1 = devm_pwm_get(&pdev->dev, "pwm1");
-  if (IS_ERR(pwm1)) {
-    int err = PTR_ERR(pwm1);
+  servo_device->pwm[1] = devm_pwm_get(servo_device->device, "pwm1");
+  if (IS_ERR(servo_device->pwm[1])) {
+    int err = PTR_ERR(servo_device->pwm[1]);
     if (err == -EPROBE_DEFER) {
       LOG_WARN("PWM not ready, deferring probe 1");
-      unregister_chrdev_region(dev, 2);
+      unregister_chrdev_region(servo_device->device_number, DEVICE_CNT);
       return -EPROBE_DEFER;
     }
-    LOG_ERROR("Failed to get PWM 1: %d", err);
-    unregister_chrdev_region(dev, 2);
+    LOG_ERROR("Failed to get PWM: %d", err);
+    unregister_chrdev_region(servo_device->device_number, DEVICE_CNT);
     return err;
   }
-
-  pwm_config(pwm0, pwm_duty_cycle_ns, pwm_period_ns);
-  pwm_config(pwm1, pwm_duty_cycle_ns, pwm_period_ns);
+  pwm_config(servo_device->pwm[1], pwm_duty_cycle_ns, pwm_period_ns);
 
   return 0;
 }
 
 int pwm_remove(struct platform_device *pdev) {
-  dev_t dev = MKDEV(major_number, 0);
-
   LOG_DEBUG("pwm_remove");
-  pwm_disable(pwm0);
-  pwm_disable(pwm1);
-  cdev_del(&servo_cdev);
-  unregister_chrdev_region(dev, 2);
+  ServoDevice *servo_device = platform_get_drvdata(pdev);
+
+  pwm_disable(servo_device->pwm[0]);
+  pwm_disable(servo_device->pwm[1]);
+  cdev_del(&servo_device->cdev);
+  unregister_chrdev_region(servo_device->device_number, DEVICE_CNT);
 
   return 0;
 }
 
 static int servo_driver_open(struct inode *inode, struct file *file_p) {
   LOG_DEBUG("servo_driver_open");
+
+  ServoDevice *servo_device = container_of(inode->i_cdev, ServoDevice, cdev);
+  unsigned int minor = MINOR(inode->i_rdev);
+
+  file_p->private_data = servo_device;
+  file_p->f_inode->i_private = (void *)(uintptr_t)minor;
+  
   return 0;
 }
 
@@ -147,14 +155,15 @@ static long servo_driver_ioctl(struct file *file_p, unsigned int cmd,
                                unsigned long arg) {
   LOG_DEBUG("servo_driver_ioctl");
 
+  ServoDevice *servo_device = file_p->private_data;
+  unsigned int minor = iminor(file_p->f_inode);
+
   switch (cmd) {
   case SERVO_ENABLE:
-    pwm_enable(pwm0);
-    pwm_enable(pwm1);
+    pwm_enable(servo_device->pwm[minor]);
     return 0;
   case SERVO_DISABLE:
-    pwm_disable(pwm0);
-    pwm_disable(pwm1);
+    pwm_disable(servo_device->pwm[minor]);
     return 0;
   default:
     LOG_WARN("unhandled ioctl command: %u", cmd);
@@ -167,11 +176,12 @@ static ssize_t servo_driver_read(struct file *file_p, char __user *buffer,
   if (*offset > 0)
     return 0;
 
-  
+  ServoDevice *servo_device = file_p->private_data;
+  unsigned int minor = iminor(file_p->f_inode);
 
   struct pwm_state state;
-  pwm_get_state(pwm0, &state);
-
+  pwm_get_state(servo_device->pwm[minor], &state);
+  
   int position = 0;
   // check if enabled, if not, write -1
   if (!state.enabled) {
@@ -180,9 +190,7 @@ static ssize_t servo_driver_read(struct file *file_p, char __user *buffer,
     position = map_value(state.duty_cycle, 500000, 2300000, 0, 180);
   }
 
-  // map value to position
-
-  char buf[16];
+  char buf[16];  // Space for 32 bit int
   const int len_written = snprintf(buf, sizeof(buf), "%u\n", position);
 
   if (copy_to_user(buffer, buf, len_written))
@@ -195,35 +203,33 @@ static ssize_t servo_driver_read(struct file *file_p, char __user *buffer,
 static ssize_t servo_driver_write(struct file *file_p,
                                   const char __user *buffer, size_t len,
                                   loff_t *offset) {
-  char buf[16];
+  ServoDevice *servo_device = file_p->private_data;
+  unsigned int minor = iminor(file_p->f_inode);
 
-  if (len >= sizeof(buf))
+  char buf[16];  // Space for 32 bit int
+  if (len >= sizeof(buf)) {
     return -EINVAL;
+  }
 
-  if (copy_from_user(buf, buffer, len))
+  if (copy_from_user(buf, buffer, len)) {
     return -EFAULT;
-
+  }
   buf[len] = '\0'; // make the input a string
 
   int val;
-  int retval = kstrtoint(buf, 10, &val);
+  int retval = kstrtoint(buf, 10 /* base */, &val);
   if (retval) {
     LOG_ERROR("kstrtoint (%d)", retval);
     return -EINVAL;
   }
 
-  // check range: should go in a config
   if (val < 0 || val > 180) {
     LOG_ERROR("servo command (%d) is out of range [%d, %d]", val, 0, 180);
     return -EINVAL;
   }
 
-  // covert to pwm
   int pwm_duty_cycle_ns = map_value(val, 0, 180, 500000, 2300000);
-
-  // write to pwm
-  pwm_config(pwm0, pwm_duty_cycle_ns, pwm_period_ns);
-  pwm_config(pwm1, pwm_duty_cycle_ns, pwm_period_ns);
+  pwm_config(servo_device->pwm[minor], pwm_duty_cycle_ns, pwm_period_ns);
   return len;
 }
 
